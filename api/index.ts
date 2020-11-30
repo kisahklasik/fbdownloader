ac26407303ecb0a200e73a7fa366bb191c9ebd77
Brama Udi <bramaudi.al@gmail.com>
2020-12-01 03:34:40 +0700

    Initial build

---
+++ api/index.ts
@@ -0,0 +1,33 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { soxa } from 'https://deno.land/x/soxa/mod.ts'
+
+const app = new Application();
+
+app.use(async (ctx) => {
+    const url = await ctx.request.url.searchParams.get('url');
+    const result = url ? await soxa.get(url) : '';
+    let body = JSON.stringify({
+        error: true,
+        message: 'URL is empty'
+    })
+    if (result) {
+        const match = /<script(.*?)>([{}].*?)<\/script>/g.exec(result.data);
+        body = match?.[2]
+            ? JSON.stringify({
+                error: false,
+                message: 'Success get content.',
+                ...JSON.parse(match?.[2])
+            })
+            : JSON.stringify({
+                error: true,
+                message: 'Can\'t found content'
+            })
+    }
+    ctx.response.headers = new Headers({
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+    })
+    ctx.response.body = body
+});
+
+export default app.handle
