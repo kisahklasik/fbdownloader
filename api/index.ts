85540b9e91db665c988689dc1d6b0c1cbf4cc3cf
Brama Udi <bramaudi.al@gmail.com>
2020-12-01 04:25:18 +0700

    Formating code

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,24 @@
-import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 import { soxa } from 'https://deno.land/x/soxa/mod.ts'
 
-const router = new Router()
+const app = new Application();
 
-router
-.get('/', async (ctx) => {
+app.use(async (ctx) => {
+
     const url = await ctx.request.url.searchParams.get('url');
     const result = url ? await soxa.get(url) : '';
+
     let body = JSON.stringify({
         error: true,
-        message: 'URL is empty'
+        message: 'URL parameter is empty'
     })
+
     if (result) {
         const match = /<script(.*?)>([{}].*?)<\/script>/g.exec(result.data);
         body = match?.[2]
             ? JSON.stringify({
                 error: false,
-                message: 'Success get content.',
+                message: 'Success get content',
                 ...JSON.parse(match?.[2])
             })
             : JSON.stringify({
@@ -24,15 +26,15 @@ router
                 message: 'Can\'t found content'
             })
     }
+
     ctx.response.headers = new Headers({
         'Access-Control-Allow-Origin': '*',
         'Accept': 'application/json',
-        'Content-type': 'application/json'
+        'Content-Type': 'application/json'
     })
-    ctx.response.body = body
-});
 
-const app = new Application();
-app.use(router.routes())
+    ctx.response.body = body
+
+});
 
 export default app.handle
