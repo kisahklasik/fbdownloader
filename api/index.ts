9675578cee77ac32a64decea5ea0127a73403322
Brama Udi <bramaudi.al@gmail.com>
2020-12-03 06:07:15 +0700

    fix: fetch dengan metode berbeda

--- api/index.ts
+++ api/index.ts
@@ -3,49 +3,54 @@ import { soxa } from 'https://deno.land/x/soxa/mod.ts'
 
 const app = new Application();
 
+const fetchPage = async (url: string, ua = false) => {
+    const config = {
+        headers: ua ? {
+            'User-Agent': 'Opera/9.80 (Android; Opera Mini/12.0.1987/37.7327; U; pl) Presto/2.12.423 Version/12.16'
+        } : {}
+    }
+    return url ? await soxa.get(url, config) : '';
+}
+
 app.use(async (ctx) => {
 
-    const config = {
-        headers: {
-            'User-Agent': 'Opera/9.80 (Android; Opera Mini/12.0.1987/37.7327; U; pl) Presto/2.12.423 Version/12.16'
-        }
-    }
     const url = await ctx.request.url.searchParams.get('url');
-    const result = url ? await soxa.get(url, config) : '';
+    let result = await fetchPage(url || '')
 
     let body = JSON.stringify({
         error: true,
         message: 'URL parameter is empty'
     })
 
-    const { data } = result
-
-    if (data) {
+    if (result.data) {
         
-        const match = /<script(.*?)>([{}].*?)<\/script>/g.exec(data);
-
+        const match = /<script(.*?)>([{}].*?)<\/script>/g.exec(result.data);
+        
         if (match?.[2]) {
 
             const output = JSON.parse(match?.[2])
+            
             body = JSON.stringify({
                 error: false,
-                message: 'Success get content',
+                message: 'Success x get content',
                 title: output?.name,
                 image: output?.thumbnailUrl,
                 url: output?.contentUrl
             })
 
         } else {
+
+            result = await fetchPage(url || '', true)
             
-            const title = /<title>(.*?)<\/title>/.exec(data)
-            const image = /<meta.?property="og:image".?content="(.*?)".?\/>/.exec(data)
-            const video = /<meta.?property="og:video".?content="(.*?)".?\/>/.exec(data)
+            const title = /<title>(.*?)<\/title>/.exec(result.data)
+            const image = /<meta.?property="og:image".?content="(.*?)".?\/>/.exec(result.data)
+            const video = /<meta.?property="og:video".?content="(.*?)".?\/>/.exec(result.data)
 
             try {
                 
                 body = JSON.stringify({
                     error: false,
-                    message: 'Success get content',
+                    message: 'Success y get content',
                     title: title?.[1],
                     image: image?.[1],
                     url: video?.[1]
