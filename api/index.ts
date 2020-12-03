78a34bbb7322db27c8f2012e7ad0d611af845b97
Brama Udi <bramaudi.al@gmail.com>
2020-12-03 08:44:59 +0700

    request dengan fetch, resonse api hanya title & url video

--- api/index.ts
+++ api/index.ts
@@ -1,70 +1,56 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { soxa } from 'https://deno.land/x/soxa/mod.ts'
 
 const app = new Application();
 
-const fetchPage = async (url: string, ua = false) => {
-    const config = {
-        headers: ua ? {
-            'User-Agent': 'Opera/9.80 (Android; Opera Mini/12.0.1987/37.7327; U; pl) Presto/2.12.423 Version/12.16'
-        } : {}
-    }
-    return url ? await soxa.get(url, config) : '';
-}
-
 app.use(async (ctx) => {
+    
+    // Set header to use Nokia Browser User-Agent
+    const config = {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NOKIAE63/1.0; Profile/MIDP-2.0 Configuration/CLDC-1.1) AppleWebKit/413 (KHTML, like Gecko) Safari/413'
+        }
+    }
 
     const url = await ctx.request.url.searchParams.get('url');
-    let result = await fetchPage(url || '')
+    const result = url ? await fetch(url, config).then(res => res.text()) : '';
 
     let body = JSON.stringify({
         error: true,
         message: 'URL parameter is empty'
     })
 
-    if (result.data) {
-        
-        const match = /<script(.*?)>([{}].*?)<\/script>/g.exec(result.data);
-        
-        if (match?.[2]) {
+    const data = await result
 
-            const output = JSON.parse(match?.[2])
+    if (data) {
+
+        const title = /<title>(.*?)<\/title>/.exec(data)
+        const video = /\?src=(.*?)['"]/.exec(data)
+
+        try {
             
             body = JSON.stringify({
                 error: false,
-                message: 'Success x get content',
-                title: output?.name,
-                image: output?.thumbnailUrl,
-                url: output?.contentUrl
+                message: 'Success get content',
+                title: title?.[1],
+                url: video?.[1] ? decodeURIComponent(video?.[1]).replace(/&amp;/g, '&') : ''
             })
 
-        } else {
-
-            result = await fetchPage(url || '', true)
+        } catch (error) {
             
-            const title = /<title>(.*?)<\/title>/.exec(result.data)
-            const image = /<meta.?property="og:image".?content="(.*?)".?\/>/.exec(result.data)
-            const video = /<meta.?property="og:video".?content="(.*?)".?\/>/.exec(result.data)
+            body = JSON.stringify({
+                error: true,
+                message: error.message
+            })
 
-            try {
-                
-                body = JSON.stringify({
-                    error: false,
-                    message: 'Success y get content',
-                    title: title?.[1],
-                    image: image?.[1],
-                    url: video?.[1]
-                })
-
-            } catch (error) {
-                
-                body = JSON.stringify({
-                    error: true,
-                    message: 'Can\'t found content'
-                })
-
-            }
         }
+
+    } else {
+
+        body = JSON.stringify({
+            error: true,
+            message: 'Can\'t found content'
+        })
+
     }
 
     ctx.response.headers = new Headers({
