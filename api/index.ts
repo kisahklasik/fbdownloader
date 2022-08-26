b9112220a44d9d57e720adf7df82e8efe1895043
Brama Udi <bramaudi.al@gmail.com>
2022-08-26 15:03:53 +0700

    Lock oak version and add dev server code

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const app = new Application();
 
@@ -70,5 +70,8 @@ app.use(async (ctx) => {
     ctx.response.body = body
 
 });
+
+// console.log('Serving on http://localhost:8000')
+// await app.listen({ port: 8000 });
 
 export default app.handle
